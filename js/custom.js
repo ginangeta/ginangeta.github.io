d7a98457f26079d8161dbbed70928174481eab7c
ginangeta <ginangeta@gmail.com>
2020-10-28 12:11:38 +0300

    Gina:Email

--- js/custom.js
+++ js/custom.js
@@ -18,4 +18,37 @@ $(function()
     $(".rotate").textrotator({
 		speed: 800
 	});
-})
+});
+
+function sendMail() {
+
+	var email_body = document.getElementById("message").value;
+	var sender_name = document.getElementById("name").value;
+	var sender_email = document.getElementById("email").value;
+
+	alert("Email Sent");
+
+    $.ajax({
+      type: 'POST',
+      url: 'https://mandrillapp.com/api/1.0/messages/send.json',
+      data: {
+        'key': 'vGb6jlgG5PkpZxZoJWDTMw',
+        'message': {
+          'from_email': sender_email,
+          'to': [
+              {
+                'email': 'ginangeta@gmail.com',
+                'name': 'Gina Wambui Ngeta',
+                'type': 'to'
+              }
+            ],
+          'autotext': 'true',
+          'subject': 'Portfolio Contact!',
+          'html': email_body
+        }
+      }
+     }).done(function(response) {
+	   console.log(response); // if you're into that sorta thing
+	   alert("Email Sent");
+     });
+}
